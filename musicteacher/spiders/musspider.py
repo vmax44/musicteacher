44509bc76833f8503f1df90e42cd9a0e7f0aec01
Vmax44 <vmax44@tut.by>
2016-09-14 01:36:40 +0300

    work on spider

--- musicteacher/spiders/musspider.py
+++ musicteacher/spiders/musspider.py
@@ -9,17 +9,21 @@ class MusspiderSpider(scrapy.Spider):
     start_urls = (
         'http://www.musicteacher.com.au/directory/',
     )
+    debug_count=0
 
-    def construct_category_req(self, node):
+    def construct_category_req(self, node, category=None, callback=None):
         url=urljoin('http://www.musicteacher.com.au',node.xpath('@href').extract_first())
-        req=scrapy.Request(url,callback=self.parse_category)
-        req.meta['category']=node.xpath('text()').extract_first()
+        req=scrapy.Request(url,callback=callback or self.parse_category)
+        req.meta['category']=category or node.xpath('text()').extract_first()
         return req
 
     def parse(self, response):
         s=scrapy.Selector(response)
         roots=s.css('div.main ul.no_bullet > li > a')
         for root in roots:
+            #self.debug_count+=1
+            #if self.debug_count>1:
+            #    break
             sub=root.xpath('parent::node()/ul/li/a')
             if len(sub)>0:
                 for a in sub:
@@ -28,6 +32,23 @@ class MusspiderSpider(scrapy.Spider):
                 yield self.construct_category_req(root)
 
     def parse_category(self, response):
+        c=scrapy.Selector(response)
+        if len(c.css("div.main > div.msg_alert"))>0:
+            yield from self.generate_from_location_reqs(response.meta['category'], c)
+        else:
+            yield from self.generate_items_urls(response.meta['category'], c)
+
+    def generate_from_location_reqs(self, category, c):
+        locations=c.css("li.two_column > a")
+        for loc in locations:
+            yield self.construct_category_req(loc, category)
+
+    def generate_items_urls(self, category, c):
+        items_urls=c.css("div.detail > div.service > a")
+        for url in items_urls:
+            yield self.construct_category_req(url,category, self.parse_item)
+
+    def parse_item(self, response):
         i=dict()
         i['category']=response.meta['category']
         i['url']=response.url
