098d0d96e122d137fa71e8eff7fb94dfdee154ac
Vmax44 <vmax44@tut.by>
2016-09-16 09:38:57 +0300

    work on spider

--- musicteacher/spiders/musspider.py
+++ musicteacher/spiders/musspider.py
@@ -9,7 +9,13 @@ class MusspiderSpider(scrapy.Spider):
     start_urls = (
         'http://www.musicteacher.com.au/directory/',
     )
+    categories={'Fiddle Lessons'} #,'Fife Lessons'}
     debug_count=0
+
+    def category_filter(self, category):
+        if len(self.categories) == 0:
+            return True
+        return category in self.categories
 
     def construct_category_req(self, node, category=None, callback=None):
         url=urljoin('http://www.musicteacher.com.au',node.xpath('@href').extract_first())
@@ -17,6 +23,7 @@ class MusspiderSpider(scrapy.Spider):
         req.meta['category']=category or node.xpath('text()').extract_first()
         return req
 
+    # parse categories from main page and yield requests
     def parse(self, response):
         s=scrapy.Selector(response)
         roots=s.css('div.main ul.no_bullet > li > a')
@@ -27,12 +34,16 @@ class MusspiderSpider(scrapy.Spider):
             sub=root.xpath('parent::node()/ul/li/a')
             if len(sub)>0:
                 for a in sub:
-                    yield self.construct_category_req(a)
+                    if self.category_filter(a.xpath('text()').extract_first()):
+                        yield self.construct_category_req(a)
             else:
-                yield self.construct_category_req(root)
+                if self.category_filter(root.xpath('text()').extract_first()):
+                    yield self.construct_category_req(root)
 
+    # parse items urls
     def parse_category(self, response):
         c=scrapy.Selector(response)
+        self.savefile(response.meta['category']+".html", response.body)
         if len(c.css("div.main > div.msg_alert"))>0:
             yield from self.generate_from_location_reqs(response.meta['category'], c)
         else:
@@ -46,6 +57,7 @@ class MusspiderSpider(scrapy.Spider):
     def generate_items_urls(self, category, c):
         items_urls=c.css("div.detail > div.service > a")
         for url in items_urls:
+            self.logger.warning("to parse %s" % url.xpath("text()").extract_first())
             yield self.construct_category_req(url,category, self.parse_item)
 
     def parse_item(self, response):
@@ -53,3 +65,8 @@ class MusspiderSpider(scrapy.Spider):
         i['category']=response.meta['category']
         i['url']=response.url
         yield i
+
+    def savefile(self, filename, content):
+        with open(filename,'w') as file:
+            print(content, file=file)
+
